5eae597dbf5425caefcf2346316590b89c4722d7
zhrbadiee <zhr.badiee@gmail.com>
2025-10-20 12:05:45 +0330

    complete MobileNav

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Nav from "./Nav";
 import { Button } from "./ui/button";
+import MobileNav from "./MobileNav";
 
 export default function Header(params) {
   return (
@@ -20,7 +21,9 @@ export default function Header(params) {
           </Link>
         </div>
         {/* mobile nav */}
-        <div className="xl:hidden">mobile nav</div>
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
